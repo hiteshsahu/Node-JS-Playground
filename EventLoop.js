f55f3c5b7caa318d2cbe89d06ec334c98bc7315e
Hitesh Sahu <hiteshkumarsahu1990@gmail.com>
2017-05-16 14:29:05 +0530

    Remove and count for number of event listeners

--- EventLoop.js
+++ EventLoop.js
@@ -9,28 +9,51 @@ var events = require('events');
 // Create an eventEmitter object
 var eventEmitter = new events.EventEmitter();
 
-// Create an event handler  and  Bind the connection event with the handler
-eventEmitter.on('connection', ()=>{
+
+ //Create event handlers
+var connectionHandler = ()=>{
   console.log('Event Handler: Connection Succesful. !!\n');
 
   console.log('Trigger Event: Transmitting Data');
   // Fire the data_received event to trigger chain of event
   eventEmitter.emit('data_received');
-});
+}
 
-// Bind the data_received event with the anonymous function
-eventEmitter.on('data_received', ()=>{
+var dataRecievedHandler = ()=>{
    console.log('Event Handler: Data received Succesfully. !!\n');
 
-   console.log('Trigger Event: Close Connection');
+   console.log(require('events').EventEmitter.listenerCount
+    (eventEmitter,'connection') + " Listner(s) listening to connection event");
+
+   console.log('Remove Handler : Removing Connection Listener ');
+   // Remove the binding of connection function
+   eventEmitter.removeListener('connection', connectionHandler);
+
+   console.log(require('events').EventEmitter.listenerCount
+    (eventEmitter,'connection') + " Listner(s) listening to connection event\n");
+
+   console.log('Trigger Event: Close Connection\n');
    // Fire the data_received event to trigger chain of event
    eventEmitter.emit('close_connection');
-});
+};
+
+var connectClosedHandler = ()=>{
+
+  console.log('Remove Handler : Removing Data Listener \n');
+  // Remove the binding of connection function
+  eventEmitter.removeListener('data_received', dataRecievedHandler);
+
+   console.log('Event Handler: Connection Closed. !!\n');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectionHandler);
 
 // Bind the data_received event with the anonymous function
-eventEmitter.on('close_connection', ()=>{
-   console.log('Event Handler: Connection Closed. !!\n');
-});
+eventEmitter.on('data_received', dataRecievedHandler);
+
+// Bind the data_received event with the anonymous function
+eventEmitter.on('close_connection', connectClosedHandler);
 
 console.log('Trigger Event: Start Connection');
 // Fire the connection event
